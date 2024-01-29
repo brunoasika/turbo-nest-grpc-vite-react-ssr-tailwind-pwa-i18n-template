773b40415b8ae4499d5f6a82756f62cc08b05b7b
brunoasikagmail.com <you@example.com>
2024-01-29 01:04:07 +0100

    question 1 and 2

--- hms-apps/users-demo-frontend/src/App.tsx
+++ hms-apps/users-demo-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+/*import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home";
 import Users from "./components/Users";
@@ -29,4 +29,59 @@ function App() {
   )
 }
 
+export default App
+*/
+import { Route, Routes } from "react-router-dom"
+import Layout from "./components/Layout"
+import Home from "./components/Home";
+import Users from "./components/Users";
+import { ThemeProvider } from "@/components/theme-provider"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { useEffect } from "react";
+import { DEV_MODE } from './global/frontend.settings';
+import { useTranslation } from 'react-i18next';
+
+function App() {
+  const baseUrl = "/v1";
+
+  const { i18n } = useTranslation();
+
+  const changeI18nLanguageToClientPreferred = async () => {
+    if (i18n.language != "")
+      await i18n.changeLanguage("en-US");
+  }
+
+  useEffect(() => {
+    //check if assetMap sent in production mode; if not, redirect to a proper ssr endpoint.
+
+    if (!DEV_MODE) {
+      //attempt to change language here to locale
+      changeI18nLanguageToClientPreferred();
+
+      // if (!assetMap) {
+      //   window.location.href = '/web'; //simulate a mouse click
+      // }
+    }
+  })
+
+  //create a react query client at the top
+  // Create a client
+  const queryClient = new QueryClient()
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Routes>
+          <Route path={`${baseUrl}`} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="view-users" element={<Users />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
 export default App
--- hms-apps/users-demo-frontend/src/main.tsx
+++ hms-apps/users-demo-frontend/src/main.tsx
@@ -1,8 +1,25 @@
+/*import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+// import i18n
+import './global/i18n.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
